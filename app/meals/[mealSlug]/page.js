f2d81230f27f98b2118e3bc5477e5765ee5728da
Rod <rodrysxc@gmail.com>
2024-08-26 00:14:08 +0200

    feat: create basic routes

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function MealDetailsPage() {
+	return (
+		<>
+			<h1>Share page</h1>
+			<p>
+				<Link href="/meal-1">Meal-1</Link>
+			</p>
+		</>
+	);
+}
